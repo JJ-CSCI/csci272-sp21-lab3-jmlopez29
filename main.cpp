7f339fae1513b74d6b315403b0206b007bfcb1df
jmlopez29 <jmlopez2992@gmail.com>
2021-03-08 00:39:48 +0000

    Edits from zoom.

--- main.cpp
+++ main.cpp
@@ -7,56 +7,42 @@
 // Put the assignment code here
 
 class Quaternion {
-  private:
   double q1;
   double q2;
   double q3;
   double q4;
 
-  public: Quaternion (double a, double b, double c, double d){
-  q1 = a;
-  q2 = b;
-  q3 = c;
-  q4 = d;
-  Quaternion bool;
-  Quaternion operator+;
-  Quaternion operator-;
-  Quaternion operator*;
-  }
+  public: Quaternion (double a, double b, double c, double d) :
+  q1{a},
+  q2{b},
+  q3{c},
+  q4{d}
+  {}
+
+  bool operator==(const Quaternion &rhs)const{ 
+    return (q1 == rhs.q1) && (q2 == rhs.q2) && ( q3 == rhs.q3) && (q4 == rhs.q4);
+  };
+
+  Quaternion operator+(const Quaternion &rhs) {
+    Quaternion res{q1+rhs.q1,q2+rhs.q2,q3+rhs.q3,q4+rhs.q4};
+    return res;
+  };
+
+  Quaternion operator-(const Quaternion &rhs) {
+    Quaternion res{q1-rhs.q1,q2-rhs.q2,q3-rhs.q3,q4-rhs.q4};
+    return res;
+  };
+
+  Quaternion operator*(double rhs){
+    Quaternion res{q1*rhs,q2*rhs,q3*rhs,q4*rhs};
+    return res;
+  };
+ 
+ friend Quaternion operator*(double lhs, const Quaternion &rhs);
 };
-bool Quaternion operator == (const Quaternion &obj){
-  if (q1==obj.q1);
-  if (q2==obj.q2);
-  if (q3==obj.q3);
-  if (q4==obj.q4);
-  Return true;
-  else
-  Return false; 
-}
-Quaternion operator + (Quaternion const &obj){
-  Quaternion evalfun; 
-  evalfun.q1 = q1 + obj.q1; 
-  evalfun.q2 = q2 + obj.q2;
-  evalfun.q3 = q3 + obj.q3;
-  evalfun.q4 = q4 + obj.q4;
-Return evalfun;
-}
-Quaternion operator - (Quaternion const &obj){
-  Quaternion evalfun; 
-  evalfun.q1 = q1 - obj.q1; 
-  evalfun.q2 = q2 - obj.q2;
-  evalfun.q3 = q3 - obj.q3;
-  evalfun.q4 = q4 - obj.q4;
-Return evalfun;
-}
-Quaternion operator * (Quaternion const &obj){
-  Quaternion evalfun; 
-  evalfun.q1 = q1 * obj.q1; 
-  evalfun.q2 = q2 * obj.q2;
-  evalfun.q3 = q3 * obj.q3;
-  evalfun.q4 = q4 * obj.q4;
-Return evalfun;
-}
+
+Quaternion operator*(double lhs, const Quaternion &rhs){Quaternion res{lhs*rhs.q1,lhs*rhs.q2,lhs*rhs.q3,lhs*rhs.q4};
+return res;
 }
 //------------------------------
 //   DO NOT MODIFY TEST CASES
@@ -66,6 +52,7 @@ TEST_CASE( "Assignment" ) {
         Quaternion q{1.0, 2.0, 3.0, 4.0};
         Quaternion p{1.0, 2.0, 3.0, 4.0};
         REQUIRE( q == p );
+        REQUIRE( q.operator==(p) );
     }
     SECTION( "q2" ) {
         Quaternion q{1.0, 2.0, 3.0, 4.0};
@@ -81,8 +68,14 @@ TEST_CASE( "Assignment" ) {
     }
     SECTION( "q3" ) {
         Quaternion q{1.0, 2.0, 3.0, 4.0};
+        Quaternion w{1.0, 2.0, 3.0, 4.0};
         Quaternion r{2.0, 4.0, 6.0, 8.0};
+        REQUIRE( q == w );
         REQUIRE( (q * 2.0) == r );
+        REQUIRE ( q == w );
+
+        REQUIRE( (2.0 * q) == r);
+
     }
 }
 //------------------------------
